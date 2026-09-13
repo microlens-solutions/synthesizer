50aed6f588b8c4fd065d59de36e4b6b573f1376f
Muhammad Mansoor Afzal <mansoorafzal@gmail.com>
2026-09-13 16:11:46 +0400

    Use candidate item

--- Microlens.Synthesizer.Core/Domain/ScaffoldResult.cs
+++ Microlens.Synthesizer.Core/Domain/ScaffoldResult.cs
@@ -2,8 +2,8 @@
 
 namespace Microlens.Synthesizer.Core.Domain;
 
-public sealed class ScaffoldResult(string input, Registry.ScaffoldStatus status) {
-    public string Input { get; set; } = input;
+public sealed class ScaffoldResult(CandidateItem input, Registry.ScaffoldStatus status) {
+    public CandidateItem Input { get; set; } = input;
 
     public Registry.ScaffoldStatus Status { get; set; } = status;
 
